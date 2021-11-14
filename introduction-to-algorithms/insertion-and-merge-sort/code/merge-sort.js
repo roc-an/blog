634e71df42204274f5eeb465bf1a7f80b27708ba
AnHongpeng <rocanhongpeng@163.com>
2021-11-14 19:42:26 +0800

    feat: 归并排序的 JavaScript 实现

--- introduction-to-algorithms/insertion-and-merge-sort/code/merge-sort.js
+++ introduction-to-algorithms/insertion-and-merge-sort/code/merge-sort.js
@@ -1,5 +1,5 @@
 /**
- * @description 归并排序
+ * @description 归并排序（升序）
  */
 const { getRandomArr } = require('./utils');
 
@@ -50,3 +50,35 @@ const getMergedArr = (arr1, arr2) => {
   }
   return distArr;
 };
+
+// 归并排序
+const getMergeSortedArray = (arr) => {
+  if (!Array.isArray(arr)) {
+    throw new Error('所提供的的数据中含有非数组元素');
+  }
+
+  const len = arr.length;
+  // 如果待归并的数组长度是奇数，那递归后一定有 1 次是 arr 就 1 个元素
+  if (len === 1) { return arr; }
+
+  // 如果 arr 有两个元素，那么比较大小后，返回升序数组
+  if (len === 2) {
+    if (arr[0] <= arr[1]) {
+      return arr;
+    } else {
+      return [arr[1], arr[0]]
+    }
+  }
+
+  // 如果 arr 超过两个元素，那么递归调用自身，取得待 merge 的两个数组
+  const midIndex = Math.floor(len/2); // 取得中间的索引，向下取整
+  const subArr1 = getMergeSortedArray(arr.slice(0, midIndex));
+  const subArr2 = getMergeSortedArray(arr.slice(midIndex, len));
+
+  // 调用归并子程序对两个子数组进行合并
+  return getMergedArr(subArr1, subArr2);
+}
+
+const arr = getRandomArr(13, 0, 10);
+console.log('待归并排序的初始数组：', arr);
+console.log('归并排序后的数组：', getMergeSortedArray(arr));
