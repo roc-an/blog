4b44fee611149c8550e4b7437b1d7ff81e6b7867
AnHongpeng <rocanhongpeng@163.com>
2021-12-09 21:08:15 +0800

    feat: 手写防抖

---
+++ a-paragraph/examples/javascript/debounce-throttle/debounce.js
@@ -0,0 +1,36 @@
+/**
+ * @description 防抖
+ */
+const input1 = document.querySelector('#input1')
+
+// let timer = null; // 定时器
+
+// input1.addEventListener('keyup', function() {
+//   // 频繁触发时，如果之前的定时器未结束，那么清空它
+//   if (timer) {
+//     clearTimeout(timer)
+//   }
+//   timer = setTimeout(() => {
+//     console.log(input1.value);
+
+//     clearTimeout(timer);
+//   }, 500)
+// })
+
+// 防抖函数 - 调用后得到新函数
+function debounce(fn, delay = 500) {
+  let timer = null;
+  return function() {
+    if (timer) {
+      clearTimeout(timer)
+    }
+    timer = setTimeout(() => {
+      fn.apply(this, arguments) // 重要，透传参数
+      clearTimeout(timer);
+    }, delay)
+  }
+}
+
+input1.addEventListener('keyup', debounce(() => {
+  console.log(input1.value)
+}, 2000))
